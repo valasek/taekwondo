a755790848b6832bcfda12a4f1085b0bdb81a4a5
Stanislav Valasek <valasek@gmail.com>
2016-10-25 20:30:31 +0200

    updated Heroku deployment files

--- itf/settings.py
+++ itf/settings.py
@@ -128,12 +128,12 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'taekwondo/staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'taekwondo/static'),
 ]
 
 # Simplified static file serving.
